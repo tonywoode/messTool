95c1ff1e81a2012693626c1de224116171ccd7db
tony woode <tonywoodecode@gmail.com>
2017-03-29 23:07:05 +0100

    tidyup doesn't work but helpful comments

--- romdataMaker.js
+++ romdataMaker.js
@@ -52,7 +52,7 @@ function makeSoftlists(callback){
       node.info = software.info
       node.sharedfeature = software.sharedfeat
       node.feature = software.part.feature
-      node.interface = software.part.$.interface
+      node.interfacer = software.part.$.interface
       softlist.push(node)
   }
   })
@@ -63,8 +63,11 @@ process.exit()
   })
 
 }
-
-
+function print(softlist){
+const tidyinfo = R.map( ({ call, cloneof, name, year, company, info, sharedfeature, feature, interfacer }) => ({ info:
+  R.map( ({$}) => ( ({name:$.name, value:$.value  }) ),info)
+}), softlist)
+}
 //function print(systems){
 //// there doesn't seem to be a way to get multiple softlists in the output for a single system, and print their properties nicely against the object. So we'll do it oursleves...
 //const printMe = R.map( ({ system, call, cloneof, softlist }) => ({ softlist: 
@@ -73,3 +76,30 @@ process.exit()
 //console.log(JSON.stringify(printMe, null, '\t'))
 //process.exit()
 //}
+//    _name, //this is the name used for display purposes
+//  _MAMEName, //Used Internally mainly for managing MAME clones.
+//  _ParentName, //Used Internally for storing the Parent of a Clone.
+//  _ZipName, //Used Internally to store which file inside a zip file is the ROM
+//  _path, //the path to the rom.
+//  _emulator, //the Emulator this rom is linked to
+//  _Company, //The company who made the game.
+//  _Year,     //the year this ROM first came out
+//  _GameType, //The type of game
+//  _MultiPlayer,  //The type of Multiplayer game.
+//  _Rating,   //A user rating.
+//  _Language, //The language of this ROM
+//  _Comment, //Any miscellaneous comments you might want to store.
+//  _Parameters : String; //Any additional paramters to be used when running
+//  _ParamMode : TROMParametersMode; //type of parameter mode
+//  _NumPlay : integer;   //The amount of times this rom has been played
+//  _DefaultGoodMerge : String; //The user selected default GoodMerge ROM
+//
+  const romdataHeader = `ROM DataFile Version : 1.1`
+
+//and a few random lines of a real romdata:
+//CD-Rom System Card (v2.1)¬cdsys¬¬¬F:\MESS\Mess\roms\pce\cdsys.zip¬MESS PC Engine -CART¬Hudson¬19??¬¬¬English¬pce -cart "%ROMMAME%" -now¬¬0¬1¬<IPS>¬</IPS>¬¬¬
+//CD-Rom System Card (v2.0)¬cdsysa¬cdsys¬¬F:\MESS\Mess\roms\pce\cdsysa.zip¬MESS PC Engine -CART¬Hudson¬19??¬¬¬English¬pce -cart "%ROMMAME%" -now¬¬0¬1¬<IPS>¬</IPS>¬¬¬
+//CD-Rom System Card (v1.0)¬cdsysb¬cdsys¬¬F:\MESS\Mess\roms\pce\cdsysb.zip¬MESS PC Engine -CART¬Hudson¬19??¬¬¬English¬pce -cart "%ROMMAME%" -now¬¬0¬1¬<IPS>¬</IPS>¬¬¬
+//Champion Wrestler¬champwrs¬¬¬F:\MESS\Mess\roms\pce\champwrs.zip¬MESS PC Engine -CART¬Taito¬1990¬¬¬English¬pce -cart "%ROMMAME%" -now¬¬0¬1¬<IPS>¬</IPS>¬¬¬
+//Taito Chase H.Q.¬chasehq¬¬¬F:\MESS\Mess\roms\pce\chasehq.zip¬MESS PC Engine -CART¬Taito¬1990¬¬¬English¬pce -cart "%ROMMAME%" -now¬¬0¬1¬<IPS>¬</IPS>¬¬¬
+
