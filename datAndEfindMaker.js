6895625e25053008d4685287791e8ef7c83e0117
tony woode <tonywoodecode@gmail.com>
2017-05-03 10:57:22 +0100

    clearup

--- datAndEfindMaker.js
+++ datAndEfindMaker.js
@@ -285,6 +285,14 @@ function mungeCompanyForType(systems){
 
 function makeFinalSystemTypes(systems){
 
+   //before we replace the clone systems with the system type they are cloned from, we need to get our type property together
+  const systemsWithType = R.map(obj => 
+    R.assoc(`systemType`, (obj.mungedCompany === `` || obj.mungedSystem === ``)? 
+      `${obj.mungedSystem}`:`${obj.mungedCompany} ${obj.mungedSystem}`, obj
+    ), systems 
+  ) 
+  // now our objects have something like the following keys  ({company, system, call, cloneof, mungedCompany, displayCompany, mungedSystem, systemType})
+
   //a function we'll pass in later that calls the clone system or reports a problem
   const lookupCall = (cloneof, call) =>  {
     const referredSystem = R.find( R.propEq(`call`, cloneof) )(systemsWithType)
@@ -295,15 +303,6 @@ function makeFinalSystemTypes(systems){
       , originalSystem.systemType
     ): referredSystem.systemType
   }
-
-  //before we replace the clone systems with the system type they are cloned from, we need to get our type property together
-  const systemsWithType = R.map(obj => 
-    R.assoc(`systemType`, (obj.mungedCompany === `` || obj.mungedSystem === ``)? 
-      `${obj.mungedSystem}`:`${obj.mungedCompany} ${obj.mungedSystem}`, obj
-    ), systems 
-  )
-
-  // now our objects have the following keys  ({company, system, call, cloneof, mungedCompany, displayCompany, mungedSystem, systemType})
 
   //change the munged system of every machine that has a cloneof property to be the system that points to: should come at the end of munging system names
   const systemsDeCloned = R.map(obj => R.assoc(`systemType`, obj.cloneof? 
