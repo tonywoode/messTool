93c9d3fc64fb935199c33ebf686284784fadef28
tony woode <tonywoodecode@gmail.com>
2017-05-05 11:40:44 +0100

    added icons for all folders

--- src/romdataMaker.js
+++ src/romdataMaker.js
@@ -625,18 +625,51 @@ function printARomdata(softlist, softlistParams) {
       
     }
 
-  return romdataLine(romParams) 
+    return romdataLine(romParams) 
 
   }, softlist)
 
   const romdata = applyRomdata(softlist)
-//console.log(romdata)
   const romdataToPrint = R.prepend(romdataHeader, romdata) 
 
   mkdirp.sync(softlistParams.outNamePath)
+  
+  /* I already did work to enable MAME icons in QuickPlay, so just print this folder config with each dat
+   *   there are 2 systems which don't have icons in the set i want, so just write an icon file for everything */
+  const iconTemplate = iconName => `
+[GoodMerge]
+GoodMergeExclamationRoms=0
+GoodMergeCompat=0
+pref1=(U) 
+pref2=(E) 
+pref3=(J) 
+
+[Mirror]
+ChkMirror=0
+TxtDir=
+LstFilter=2A2E7A69700D0A2A2E7261720D0A2A2E6163650D0A2A2E377A0D0A
+
+[RealIcon]
+ChkRealIcons=0
+ChkLargeIcons=0
+
+[BkGround]
+ChkBk=0
+TxtBKPath=
+
+[Icon]
+ChkIcon=1
+CmbIcon=${iconName}.ico
+`
+
+  const machineMameName = softlistParams.thisEmulator.call
+  fs.writeFileSync(`${softlistParams.outNamePath}/folders.ini`, iconTemplate(machineMameName))
+  fs.writeFileSync(`${softlistParams.outTypePath}/folders.ini`, iconTemplate(machineMameName)) //last wins is fine
+  fs.writeFileSync(`${softlistParams.outRootDir}/folders.ini`, iconTemplate(machineMameName)) //last wins is fine
+
+
   fs.writeFileSync(softlistParams.outFullPath, romdataToPrint.join(`\n`), `latin1`) //utf8 isn't possible at this time
-  //console.log(JSON.stringify(softlist, null, '\t'))
+  
   return softlist
 
 }
-
