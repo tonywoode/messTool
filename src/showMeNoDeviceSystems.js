65639f0c88508cb0f17cb6eb2901274599ca25ec
tony woode <tonywoodecode@gmail.com>
2017-05-08 22:50:20 +0100

    improved company dupe regex

--- src/showMeNoDeviceSystems.js
+++ src/showMeNoDeviceSystems.js
@@ -63,12 +63,30 @@ function makeSystems(callback){
 }
 
 function mungeCompanyAndSystemNames(systems){
-   //These are the main replacement functions to munge MESS' company name and system name
+
+  //the first two of these regexs are unique to this script. That's because, to describe a generic 
+  //'space invaders' hardware,  it seems you have no option but to get company name in there
+ const systemsAugmented = R.pipe(
+   R.map(obj => R.assoc(`system`, obj.system.replace(
+          new RegExp(`\\W\\(` + obj.company.split(spaceIsSeparator, oneWord) + `\\)`, `i`), ``
+      ), obj //`Casio Game (Casio)` -> `Casio Game` 
+    ))
+, R.map(obj => R.assoc(`system`, obj.system.replace(
+          new RegExp(obj.company.split(spaceIsSeparator, oneWord) + `\\W` + `\\s`, `i`), ``
+      ), obj  //`Casio Game (Casio, v12)` -> `Casio Game`
+    )) 
+, R.map(obj => R.assoc(`system`, obj.system.replace(
+          new RegExp(obj.company.split(spaceIsSeparator, oneWord) + `\\W`, `i`), ``
+      ), obj // `Casio Casio Mk3` ->`Casio Mk3g`
+    )) 
+ )(systems)
+
+
+
   const compRep = (oldCompany, newCompany) => R.map( 
     obj => R.assoc(`company`, obj.company.replace(oldCompany, newCompany), obj) 
   )
 
-  //we match the company too when replacing the systeem name
   const systRep = (thisCompany, oldsystem, newsystem) => R.map( 
     obj => R.assoc(`system`, (obj.company.match(thisCompany) && obj.system.match(oldsystem))? 
       newsystem : obj.system, obj
@@ -79,17 +97,16 @@ function mungeCompanyAndSystemNames(systems){
   const mungedSystems = R.pipe(
 
       compRep(/(<unknown>|<generic>)/, ``)
-      //system specific (btw replace accepts regex or string by default (i'm trying to show what's intended), but match matches only regex
     , systRep(`Acorn`, /BBC/, `BBC`), systRep(`Acorn`, /Electron/, `Atom`)
     , compRep(`Hegener & Glaser Muenchen`, `Hegener & Glaser`)
-    , compRep(`John L.Weinrich`, `Weinrich`)
-    , compRep(`JAKKS Pacific Inc / HotGen Ltd `, `JAKKS / Hotgen`)
+    , compRep(`John L. Weinrich`, `Weinrich`)
+    , compRep(`JAKKS Pacific Inc / HotGen Ltd`, `JAKKS / Hotgen`)
     , compRep(`Commodore Business Machines`, `Commodore`)
     , compRep(`Elector Electronics`, `Elektor`)
     , compRep(`APF Electronics Inc.`, `APF`)
     , compRep(`VEB Elektronik`, `VEB`)
     , compRep(`San Bergmans & Izabella Malcolm`, `Bergmans & Malcolm`)
-    )(systems)
+    )(systemsAugmented)
 
   return mungedSystems
 }
