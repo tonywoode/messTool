9e6de2cb0063d896ab845799a96bbd1d972f5b4c
tony woode <tonywoodecode@gmail.com>
2017-05-10 18:45:01 +0100

    reintroduce retroarch embedded
    (in case the failiure is down to fileset mismatch)

--- src/embeddedSystems.js
+++ src/embeddedSystems.js
@@ -155,8 +155,9 @@ function printARomdata(systems) {
   const mameRomdataLine = ({name, MAMEName, parentName, path, company, year, comment}) =>
     ( `${name}¬${MAMEName}¬${parentName}¬¬${path}¬Mame64 Win32¬${company}¬${year}¬¬¬¬¬${comment}¬0¬1¬<IPS>¬</IPS>¬¬¬` )
 
-  //this is the correct invocation for retroarch but it doesn't work, even with softlist automedia off and bios anable on
-  // retroarch_debug shows it even finds the game, but then decides: 'Error: unknown option: sfach'
+  /* this is the correct invocation for retroarch but it doesn't work, even with softlist automedia off and bios enable on
+   * retroarch_debug shows it even finds the game, but then decides: 'Error: unknown option: sfach'. I've left it in in case
+   * it might be related to a mismatch in MAME versions between my fileset and retroarch */
   const retroarchRomdataLine = ({name, MAMEName, parentName, path, company, year, comment}) =>
     ( `${name}¬${MAMEName}¬${parentName}¬¬${path}¬Retroarch Frontend¬${company}¬${year}¬¬¬¬-L cores\\mame_libretro.dll " ${MAMEName.replace(/"/g, '\\"')}"¬${comment}¬0¬1¬<IPS>¬</IPS>¬¬¬` )
   /*  1)  Display name, 2) _MAMEName, 3) _ParentName, 4) _ZipName, //Used Internally to store which file inside a zip file is the ROM
@@ -181,12 +182,13 @@ function printARomdata(systems) {
   }, systems)
 
   const mameRomdata = applyRomdata("mame")
-  const retroarchRomdata = applyRomdata("retroarch")
+  //const retroarchRomdata = applyRomdata("retroarch")
   const mameRomdataToPrint = R.prepend(romdataHeader, mameRomdata) 
-  const retroarchRomdataToPrint = R.prepend(romdataHeader, retroarchRomdata) 
+  //const retroarchRomdataToPrint = R.prepend(romdataHeader, retroarchRomdata) 
   const mameSoftRoot = `outputs/mame_softlists/`
   const mameOut = `${mameSoftRoot}/MESS Embedded Systems/`
-  //const retroarchOut = `outputs/retroarch_embedded/` - not working in retroarch at this time
+  //const retroarchSoftRoot = `outputs/retroarch_softlists/`
+  //const retroarchOut = `${retroarchSoftRoot}/MESS Embedded Systems/`
   mkdirp.sync(mameOut)
   //mkdirp.sync(retroarchOut)
 
