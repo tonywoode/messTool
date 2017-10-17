952461db51c2c9a5b06e0cd7786a7a0e1d8317c6
tony woode <tonywoodecode@gmail.com>
2017-10-17 21:32:23 +0100

    split xml reader into own file

--- src/datAndEfindMaker.js
+++ src/datAndEfindMaker.js
@@ -1,10 +1,11 @@
 "use strict"
 
 const 
-    fs            = require(`fs`)
-  , readline      = require(`readline`)
-  , XmlStream     = require(`xml-stream`)
-  , R             = require(`ramda`)
+    fs            = require('fs')
+  , readline      = require('readline')
+  , R             = require('ramda')
+const XmlStream     = require(`xml-stream`)
+const makeSystems= require('./readMameXML.js')
 
 const 
     datInPath        = `inputs/systems.dat`
@@ -32,7 +33,7 @@ const rl = readline.createInterface({ input: fs.createReadStream(datInPath) })
 rl.on( 'line', (line) => currentTypeList.push(line) )
 
 //program flow
-makeSystems( systems => {
+makeSystems(  xml, systems => {
 
   R.pipe(
      cleanSoftlists
@@ -54,46 +55,6 @@ function mockSystems(callback) {
   callback(systems, callback)
 }
 
-//Parse the mame xml pulling out the fields we need but only from systems which actually work
-function makeSystems(callback) {
-  const systems = []
-  
-  //xml stream 'collects' these meaning it deals with repeating xml keys rather than overwriting each time
-  xml.collect('device')
-  xml.collect('softwarelist')
-  xml.collect(`extension`) //turns out xml stream is just regexing these keys, so this is deeply-nested
-  console.log(`Reading a very large xml file, patience...`) 
-  xml.on(`updateElement: machine`, machine => {
-    if ( //machine.softwarelist // we used to do this when doing retroarch, but it turned out life wasn't that simple after all....
-         machine.device //this helps to narrow down on MESS machines vs Arcade games (lack of coin slots isn't quite enough, but this isn't enough either as many arcade machines had dvd drives)
-      && machine.$.isdevice         === `no` //see the mame.exe (internal)  DTD which defaults to no: <!ATTLIST machine isdevice (yes|no) "no"> TODO: some home consoles didn't have devices...
-      && machine.$.isbios           === `no` 
-      && machine.$.ismechanical     === `no`
-      && machine.$.runnable         === `yes`
-      && !machine.input.$.coins
-      //&& machine.driver.$.status  === `good` //I think this is some kind of intersection of the some or all of the below
-      && machine.driver.$.emulation === `good`
-      //&& machine.driver.$.color   === `good`
-      //&& machine.driver.$.sound   === `good`
-      //&& machine.driver.$.graphic === `good` //you want nes? don't turn this on....
-    ) {
-      const node    = {}
-      node.company  = machine.manufacturer
-      node.system   = machine.description 
-      node.call     = machine.$.name
-      node.cloneof  = machine.$.cloneof
-      node.softlist = machine.softwarelist
-      node.device   = machine.device
-      systems.push(node)
-    }
-  })
-
-  xml.on(`end`, () => {
-    //fs.writeFileSync(`inputs/systems.json`, JSON.stringify(systems, null, `\t`)); process.exit()
-    callback(systems)
-  })
-
-}
 
 
 //I don't like working with a messy tree, lots of $ and needless repetition...
